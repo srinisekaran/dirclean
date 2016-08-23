a2f4ec26afd904641a237cd8fae2dc5a8a7a03cd
Srinivaas Sekaran <ssrinivaas@users.noreply.github.com>
2016-08-23 14:05:42 -0700

    Add peek function

--- dirclean.sh
+++ dirclean.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 # CLI recycle bin for individual directories
 # Supply a file extension to recycle certain file types
-# Ability to put back files and permanently delete
+# Ability to put back files, view recycle bin, and permanently delete
 # Place bash script in the directory to clean up
 
 DIR="$(basename "$PWD"_trash)"
@@ -27,17 +27,24 @@ delete(){
   fi   
 }
 
+peek(){
+  echo "Files in recycle bin:"
+  ls $DIR/
+}
+
 if [ "$#" -ne "0" ]; then 
   if [ "$1" == "undo" ]; then  
     undo
   elif [ "$1" == "delete" ]; then
     delete
+  elif [ "$1" == "peek" ]; then
+    peek
   else
     mv *.$* $DIR/ || echo "No such files found!"
   fi 
 
 else 
-  echo usage: `basename $0` "[file] [undo] [delete]" 1>&2
+  echo usage: `basename $0` "[file] [undo] [delete] [peek]" 1>&2
 fi 
 
 
